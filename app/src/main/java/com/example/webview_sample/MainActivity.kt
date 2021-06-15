65390c2a1f97a8bf87529d038741e035a6e8e6e9
Sk1droWWW <Skidrow132879@gmail.com>
2021-06-15 15:47:35 +0300

    feat: implement webView

--- app/src/main/java/com/example/webview_sample/MainActivity.kt
+++ app/src/main/java/com/example/webview_sample/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.example.webview_sample
 
+import android.annotation.SuppressLint
+import android.annotation.TargetApi
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+    private val DESTINATION_URL = "https://translate.google.com/"
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+        webView.webViewClient = MyWebViewClient()
+        webView.settings.javaScriptEnabled = true
+
+        webView.loadUrl(DESTINATION_URL)
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+}
+
+private class MyWebViewClient : WebViewClient() {
+    @TargetApi(Build.VERSION_CODES.N)
+    override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+        view.loadUrl(request.url.toString())
+        return true
+    }
+
+    // Для старых устройств
+    override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+        view.loadUrl(url)
+        return true
     }
 }
